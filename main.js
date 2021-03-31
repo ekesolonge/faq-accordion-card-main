e32256af8e2b20619e201656bd5e94bd3ea333c4
Ekeso <ekesolonge@gmail.com>
2021-03-31 22:23:45 +0100

    update

--- main.js
+++ main.js
@@ -4,14 +4,19 @@ question.forEach((question) => {
   question.addEventListener("click", (e) => {
     e.target.parentNode.parentNode.children[1].classList.toggle("show");
 
-    if (e.target.parentNode.children[1].children[0].style.transform == "")
-      return (e.target.parentNode.children[1].children[0].style.transform =
-        "rotate(180deg)");
+    if (e.target.parentNode.children[1].children[0].style.transform == "") {
+      e.target.parentNode.children[1].children[0].style.transform =
+        "rotate(180deg)";
+      e.target.parentNode.children[0].style.fontWeight = 700;
+      return;
+    }
 
     if (
       e.target.parentNode.children[1].children[0].style.transform ===
       "rotate(180deg)"
     )
-      return (e.target.parentNode.children[1].children[0].style.transform = "");
+      e.target.parentNode.children[1].children[0].style.transform = "";
+    e.target.parentNode.children[0].style.fontWeight = "";
+    return;
   });
 });
